e2004ab8fd1906f6387788e192d6444f11931451
Ofir Ben Shahar <oophir1@gmail.com>
2016-09-06 19:39:32 +0300

    Added basic io classes

---
+++ my algorithms/src/io/MyCompressorOutputStream.java
@@ -0,0 +1,44 @@
+package io;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class MyCompressorOutputStream extends OutputStream {
+
+	private OutputStream out;
+	
+	public MyCompressorOutputStream(OutputStream out) {
+		this.out = out;
+	}
+	
+	@Override
+	public void write(int b) throws IOException {
+		out.write(b);
+	}
+	
+	@Override
+	public void write(byte[] arr) throws IOException {
+		byte currByte = arr[0];
+		int count = 1;
+		
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] != currByte) {
+				while (count >= 255) {
+					out.write(255);
+					out.write(currByte);
+					count -= 255;
+				}
+				out.write(count);
+				out.write(currByte);
+				currByte = arr[i];
+				count = 1;
+			}
+			else {
+				count++;
+			}
+		}
+		out.write(count);
+		out.write(currByte);
+		
+	}
+}
