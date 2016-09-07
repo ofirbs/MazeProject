0e07a3aa8a4f05d1a5e3f2d67134b40f4e3a0400
Ofir Ben Shahar <oophir1@gmail.com>
2016-09-07 19:32:50 +0300

    added basic CLI and Command

--- cli/src/view/CLI.java
+++ cli/src/view/CLI.java
@@ -1,5 +1,20 @@
 package view;
 
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+
 public class CLI {
+	private BufferedReader in;
+	private PrintWriter out;
 	
+	public CLI(BufferedReader in, PrintWriter out) {
+		super();
+		this.in = in;
+		this.out = out;
+	}
+	
+	private void start()
+	{
+		
+	}
 }
